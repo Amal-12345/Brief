61945ffe7e7e98012e0d66c96280b8838003ca39
Amal <amal.nachit95@hotmail.com>
2024-01-22 09:02:44 +0100

    Amal

--- style/script.js
+++ style/script.js
@@ -1,3 +1,8 @@
-window.onload = () => {
-    
+const hamburgerButton = document.querySelector (".nav-toggler")
+const navigation = document.querySelector ("nav")
+hamburgerButton.addEventListener("click", toggleNav)
+
+function toggleNav(){
+    hamburgerButton.classList.toggle("active")
+    navigation.classList.toggle("active");
 }
