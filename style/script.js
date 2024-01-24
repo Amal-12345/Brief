62abbfa40283c4720d862ddca3345c12884b81a0
Rodrigo Ferreira <rodrigo.ferreira.simplon@gmail.com>
2024-01-24 14:55:12 +0100

    commit

---
+++ style/script.js
@@ -0,0 +1,14 @@
+const hamburgerButton = document.querySelector(".nav-toggler");
+const navigation = document.querySelector("nav");
+hamburgerButton.addEventListener("click", toggleNav);
+
+function toggleNav() {
+  hamburgerButton.classList.toggle("active");
+  navigation.classList.toggle("active");
+}
+
+// function darkMode() {
+//   let body = document.getElementById("main");
+
+//   body.classList.toggle("main");
+// }
