774852ece4dc0c7168d24096249e948b06197004
Amal <amal.nachit95@hotmail.com>
2024-01-29 13:34:03 +0100

    septieme commit

--- js/script.js
+++ js/script.js
@@ -7,6 +7,28 @@ function toggleNav() {
   navigation.classList.toggle("active");
 }
 
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+
+
 // function darkMode() {
 //   let body = document.getElementById("main");
 
