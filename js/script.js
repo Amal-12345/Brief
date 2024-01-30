5010a748c37628f8ddf3fdc102b3b6c98d46c615
Rodrigo Ferreira <rodrigo.ferreira.simplon@gmail.com>
2024-01-30 16:49:31 +0100

    Commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,40 @@
 const hamburgerButton = document.querySelector(".nav-toggler");
 const navigation = document.querySelector("nav");
 hamburgerButton.addEventListener("click", toggleNav);
-
 function toggleNav() {
   hamburgerButton.classList.toggle("active");
   navigation.classList.toggle("active");
 }
 
-// function darkMode() {
-//   let body = document.getElementById("main");
+// Get the button:
+let mybutton = document.getElementById("myBtn");
 
-//   body.classList.toggle("main");
-// }
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 1000 ||
+    document.documentElement.scrollTop > 1000
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function darkMode() {
+  let body = document.getElementById("main");
+  let rtbyc = document.getElementById("RTBYC");
+  let color = document.getElementById("mainP");
+  body.classList.toggle("main");
+  rtbyc.classList.toggle("RTBYC");
+  color.classList.toggle("mainP");
+}
